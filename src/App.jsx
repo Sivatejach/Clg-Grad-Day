5b93abd3001888198806458bdab0c9b29d61e101
Sivatejach <chsteja2003@gmail.com>
2024-07-29 15:33:52 +0530

    Modal Image Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Import the CSS file
 import FormComponent from './Components/FormComponent';
 import ModalComponent from './Components/ModalComponent';
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowModal(true);
-    }, 2500); // Show modal after 2.5 seconds
-
-    return () => clearTimeout(timer); // Cleanup timer on component unmount
-  }, []);
+  const [showModal, setShowModal] = useState(true);
 
   const handleClose = () => setShowModal(false);
 
--- src/Components/ModalComponent.jsx
+++ src/Components/ModalComponent.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-
 import './ModalComponent.css';
+import graduationImage from '../assets/18433.jpg'; // Adjust the path as necessary
 
 const ModalComponent = ({ showModal, handleClose }) => {
   return (
     <Modal show={showModal} onHide={handleClose} dialogClassName="modal-90w">
       <Modal.Header closeButton>
-        <Modal.Title>Scrolling Long Content Modal</Modal.Title>
+        <Modal.Title className="modal-title">Graduation Day</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {/* Long content to enable scrolling */}
         <div className="modal-body-content">
-          <p>Woohoo, you're reading this text in a modal! </p>
-          {Array.from({ length: 10 }, (_, i) => (
-            <p key={i}>This is some long content... {i + 1}</p>
-          ))}
+          <img src={graduationImage} alt="Graduation Day" className="graduation-image" />
         </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
+      
     </Modal>
   );
 };
